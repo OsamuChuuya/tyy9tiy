96159510116d7bb05eb39d07974f31c104bd7eef
OsamuChuuya <osamuchuuya26@gmail.com>
2024-12-18 22:51:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+alert(`right now will be random number`)
+let randomNumber = Math.floor( Math.random()*10)+1;
+alert(` Random numbre ${randomNumber}`)
+alert(`Biz random sonni 2ga kopaytirmoqchimiz va bolmoqchimiz!`)
+let result1 = randomNumber * 2;
+let result2 = randomNumber / 2;
+alert(`Random son: ${randomNumber} edi \n biz uni 2ga ko'paytirib ${result1} sonini chiqardik \n va shuni 2ga bo'lib: ${result2} sonini chiqardik` )
